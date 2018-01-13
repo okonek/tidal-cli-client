c11f79f360df7281d4fd78dc6b12bb0b6789cd2f
okonek <jandominikokonski@gmail.com>
2018-01-13 18:46:59 +0100

    Changed config directory

--- app/StartApp.js
+++ app/StartApp.js
@@ -2,7 +2,7 @@ const TidalApi = require("./TidalApi");
 let MainScreen = require("./UI/MainScreen");
 
 module.exports = function () {
-    const config = require(process.env.HOME + "/.config/tidalConfig.js");
+    const config = require("/etc/tidalConfig.js");
 
     const tidalApi = new TidalApi(config);
     let mainScreen = new MainScreen(tidalApi);
