2befed19106364958e712c27a94370827f718209
okonek <jandominikokonski@gmail.com>
2018-04-03 14:47:41 +0000

    Changed artist top tracks tracks limit from 10 to 100

--- app/TidalApi.js
+++ app/TidalApi.js
@@ -72,7 +72,7 @@ module.exports = class TidalApi extends Api {
 
     getArtistTopTracks(artist) {
         return new Promise((resolve) => {
-            this.getTopTracks({id: artist.id, limit: 10}, (tracks) => {
+            this.getTopTracks({id: artist.id, limit: 100}, (tracks) => {
                 resolve(tracks.items.map((trackObject) => new Track(trackObject)));
             });
         });
